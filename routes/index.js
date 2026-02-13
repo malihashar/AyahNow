faebfa357bf7cf1a9ac3f20ad02dc8f1d77bfb2c
Muhammad Ali <alihashar0406@gmail.com>
2026-02-13 16:12:32 -0500

    route added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello' });
+app.use(express.static("public"));
+
+router.get('/', aysnc function(req, res, next) {
+  const rndmayah= Math.floor((Math.random()*6,236)+1)
+   const response = await axios.get(
+      `https://api.alquran.cloud/v1/ayah/${randomAyah}/editions/quran-uthmani,en.sahih`
+    );
+    const data=response.data.data
+    const arabic=data[0]
+    const eng=data[1]
+
+    res.json({
+      surah: arabic.surah.englishName,
+      surahArabic: arabic.surah.name,
+      ayahNumber: arabic.numberInSurah,
+      arabicText: arabic.text,
+      englishText: english.text,
+    })
+
+  res.render('index');
 });
 
 module.exports = router;
