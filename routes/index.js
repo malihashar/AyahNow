b19b41ee498ca38d9d8204233beeee08573827d0
Muhammad Ali <alihashar0406@gmail.com>
2026-02-13 16:56:43 -0500

    finsihed frontend

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 const axios = require("axios");
-const cors = require("cors");
 
-const app = express();
-
-app.use(cors());
-/* GET home page. */
-app.use(express.static("public"));
-
-router.get('/', aysnc function(req, res, next) {
-  const rndmayah= Math.floor((Math.random()*6,236)+1)
+router.get("/",  function(req,res){
+  res.render('index')
+})
+router.get('/random-ayah', async function(req, res, next) {
+  const randomAyah= Math.floor((Math.random()*6236)+1)
    const response = await axios.get(
       `https://api.alquran.cloud/v1/ayah/${randomAyah}/editions/quran-uthmani,en.sahih`
     );
     const data=response.data.data
     const arabic=data[0]
-    const eng=data[1]
+    const english=data[1]
 
     res.json({
       surah: arabic.surah.englishName,
@@ -27,7 +23,7 @@ router.get('/', aysnc function(req, res, next) {
       englishText: english.text,
     })
 
-  res.render('index');
+ 
 });
 
 module.exports = router;
